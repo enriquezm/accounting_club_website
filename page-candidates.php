c4d2828c9ea3b9fcc6a70b22960f479b3dde7cd3
Myles Enriquez <omarenri@hawaii.edu>
2015-11-08 23:36:56 -1000

    Create page template for candidates.

---
+++ page-candidates.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 	Template Name: Candidates Template
+ *
+ * @package Club Website
+ */
+get_header();
+?>
+
+	<div id="candidates" class="content-area">
+		<h1>Candidates</h1>
+	</div><!-- #candidates -->
+<?php get_footer(); ?>
