bd205f39ef788c7d6e7d2e1aa3df0bb017be3a65
Myles Enriquez <omarenri@hawaii.edu>
2015-11-10 01:02:08 -1000

    Clean out and remove unused code.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-$(document).ready(function(){
-	//console.log("helloworld test"); // for testing purposes
+jQuery(document).ready(function(){
+	// console.log("helloworld test"); // for testing purposes
 
-
-	//Giving anchor scrolling some animation (ease).
-	//Increased performance by caching $('html, body') selector
-	var $root = $('html, body');
-
-	$("a").click(function(){
-		$root.animate({
-			scrollTop: $( $.attr(this, 'href') ).offset().top
-		}, 1000);
-		return false;
-	});
 });
